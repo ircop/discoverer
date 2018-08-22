0a4729108655885fcc533a815060c4d735414e73
ircop <wingman@ip-home.net>
2018-08-22 21:49:33 +0300

    cli: parsing tables

---
+++ util/text/table.go
@@ -0,0 +1,177 @@
+package text
+
+import (
+	"regexp"
+	"strings"
+)
+
+// ParseTable - we will try to parse tables only with headers.
+//
+// Headers examples:
+// `Port      Name               Status       Vlan       Duplex  Speed Type`
+// `--------- ----------------- ------------- ----------------- ------------ -----`
+//
+// 1) Split header by first non-space char
+// 2) Loop trough splitted parts. Calculate length between first and last (with spaces): this would be max. column len
+// 3) Loop over next lines
+// 4) If line is empty, or line is footer, break loop
+// 5) for range columns { Split lines by column len. Last column should has unlimited len! }
+//
+// t: text to parse
+// header: header matching regex
+// footer: footer matching regex
+func ParseTable(t string, header string, footer string) [][]string {
+	rows := make([][]string, 0)
+
+	// we NEED a header regex
+	if header == "" {
+		return rows
+	}
+
+	var reHeader, reFooter *regexp.Regexp
+
+	reHeader, err := regexp.Compile(header)
+	if err != nil {
+		return rows
+	}
+	if footer != "" {
+		reFooter, err = regexp.Compile(footer)
+		if err != nil {
+			return rows
+		}
+	}
+
+	var started bool
+	lines := strings.Split(t, "\n")
+	var columnsLengths []int64
+
+	for _, line := range lines {
+		if !started {
+			if reHeader.Match([]byte(line)) {
+				started = true
+				// parse header
+				columnsLengths = parseHeader(line)
+			} else {
+				continue
+			}
+		} else {
+			// We are started already.
+			// Parse current line regarding to columns parameters.
+
+			// But first - stop loop, if we have empty line or footer match
+			if strings.Trim(line, " ") == "" || (footer != "" && reFooter.Match([]byte(line))) {
+				break
+			}
+
+			cols := parseLine(line, columnsLengths)
+			// assuming first column is always non-empty...
+			// If it's empty, we assuming this is continuous previous row
+			cols[0] = strings.Trim(cols[0], " ")
+			if cols[0] != "" {
+				rows = append(rows, cols)
+				continue
+			}
+			// empty first column. Loop trough all rows, append cur to prev row
+			// if there is no prev.row, we don't know what to do
+			if len(rows) == 0 {
+				continue
+			}
+			lastRowIdx := len(rows)-1
+			for i := range cols {
+				rows[lastRowIdx][i] += cols[i]
+			}
+		}
+	}
+
+	// finally, just loop trough all rows/cols and trim them
+	for i := range rows {
+		for j := range rows[i] {
+			rows[i][j] = strings.Trim(rows[i][j], " ")
+		}
+
+		// We have some exclusions :( Like cisco 'sh int status': 'trunk      a-full', where 'a' belongs to previous field :(
+		if len(rows[i]) > 5 {
+			c4 := rows[i][4]
+			c5 := rows[i][5]
+			if strings.HasSuffix(c4, " a") && strings.HasPrefix(c5, "-") {
+				rows[i][4] = strings.Trim(c4[:len(c4)-2], " ")
+				rows[i][5] = "a" + c5
+			}
+		}
+	}
+
+	return rows
+}
+
+func parseLine(line string, columnLengths []int64) []string {
+	cols := make([]string, 0)
+
+	chars := strings.Split(line, "")
+	for colIdx, colLen := range columnLengths {
+		// no chars left, add empty column
+		if len(chars) == 0 {
+			cols = append(cols, "")
+			continue
+		}
+
+		// col lenght is more then chars left. Append existing chars and strip them.
+		if colLen >= int64(len(chars)) {
+			col := strings.Join(chars, "")
+			cols = append(cols, col)
+			chars = make([]string, 0)
+			continue
+		}
+
+		// If this is last column, append all chars and break
+		if colIdx == len(columnLengths) - 1 {
+			col := strings.Join(chars, "")
+			cols = append(cols, col)
+			break
+		}
+
+		// we have enough chars, append colLen chars to columns
+		col := strings.Join(chars[:colLen], "")
+		// we should NOT tim first spaces, but should trim last ones
+		col = strings.TrimRight(col, " ")
+		cols = append(cols, col)
+		chars = chars[colLen:]
+	}
+
+	return cols
+}
+
+func parseHeader(header string) []int64 {
+	columns := make([]int64, 0)
+
+	header = strings.Trim(header, "\n")
+	var curColumn int64
+	chars := strings.Split(header, "")
+	columnTextStarted := false
+	wasNonSpaces := false
+	for _, c := range chars {
+		if c == " " {
+			// just add this char to column len
+			curColumn++
+			columnTextStarted = false
+		} else {
+			// If this is FIRST non-space character, this is probably next column
+			// If not first, this is first part of column
+			if columnTextStarted || !wasNonSpaces {
+				curColumn++
+				wasNonSpaces = true
+				columnTextStarted =  true
+			} else {
+				// non-space: this is next column start. BUT only if this is not 1st non-space character in whole string.
+				columns = append(columns, curColumn)
+				curColumn = 1
+				columnTextStarted = true
+			}
+		}
+	}
+	// if last column was stopped without spaces, append it also
+	if curColumn > 0 && columnTextStarted {
+		columns = append(columns, curColumn)
+	}
+
+	return columns
+}
---
+++ util/text/table_test.go
@@ -0,0 +1,111 @@
+package text
+
+import (
+	"testing"
+)
+
+func Test_ParseShIntStatus(t *testing.T) {
+	output := `sh int status
+
+Port      Name               Status       Vlan       Duplex  Speed Type
+Gi1/1     -= ATC =-          connected    trunk        full   1000 1000BaseLH
+Gi1/2     DXS-112.10         connected    trunk        full   1000 1000BaseLH
+Gi1/3     DGS-10.170.112.253 connected    trunk        full   1000 1000BaseBX10-D
+Gi1/4     po3:DGS-113.154    connected    trunk        full   1000 1000BaseLH
+Gi1/5     DXS-10.170.113.132 notconnect   1            full   1000 No Gbic
+Gi1/6     DGS 10.170.113.131 connected    trunk        full   1000 1000BaseBX10-D
+Gi1/7     DXS-112.10         connected    trunk        full   1000 1000BaseLH
+Gi1/8     Po6 <-> Port 9 DGS connected    trunk        full   1000 1000BaseLH
+Gi1/9     DXS-10.170.113.132 connected    trunk        full   1000 Unknown GBIC
+Gi1/10    DXS-10.170.113.132 connected    trunk        full   1000 Unknown GBIC
+Gi1/11    DGS-10.170.112.253 connected    trunk        full   1000 Unknown GBIC
+Gi1/12    DGS-10.170.111.48  connected    trunk        full   1000 Unknown GBIC
+Gi1/13    DGS-10.170.113.155 connected    trunk        full   1000 1000BaseLH
+Gi1/14    DGS-10.170.111.116 connected    trunk        full   1000 Unknown GBIC
+Gi1/15    50-let-VLKSM-12    connected    trunk        full   1000 1000BaseBX10-D
+Gi1/16    DGS 10.170.113.100 connected    trunk        full   1000 1000BaseLH
+Gi1/17    DGS 10.170.111.157 connected    trunk        full   1000 1000BaseLH
+Gi1/18    po3:DGS-113.154    connected    trunk        full   1000 1000BaseLH
+Gi1/19    Po1:DGS-10.170.111 connected    trunk        full   1000 1000BaseLH
+Gi1/20    Po1:DGS-10.170.111 connected    trunk        full   1000 1000BaseLH
+Gi1/21    Po4:111.250        connected    trunk        full   1000 1000BaseLH
+Gi1/22    Po4:111.250        connected    trunk        full   1000 1000BaseLH
+Gi1/23    Po4:111.250        connected    trunk        full   1000 1000BaseLH
+Gi1/24    Po4:111.250        connected    trunk        full   1000 1000BaseLH
+Gi1/25    Po7:10.170.114.249 connected    trunk        full   1000 1000BaseLH
+Gi1/26    Po6 <-> Port 24 DG notconnect   731          full   1000 1000BaseLH
+Gi1/27    Po7:10.170.114.249 connected    trunk        full   1000 1000BaseLH
+Gi1/28    -= 10.170.111.48 = connected    trunk        full   1000 Unknown GBIC
+Te1/29    -= RAM =-          connected    trunk        full    10G 10GBase-LR
+Te1/30    -= Shatura =-      connected    trunk        full    10G 10GBase-LR
+Po1       DGS-111.114        connected    trunk      a-full a-1000
+Po2       DGS-112.60         notconnect   0            auto   auto
+Po3                          connected    trunk      a-full a-1000
+Po4       DGS-111.250        connected    trunk      a-full a-1000
+Po5       DXS-112.10         connected    trunk      a-full a-1000
+
+`
+
+	rows := ParseTable(output, `^Port\s+Name`, "")
+	if len(rows) != 35 {
+		t.Fatal("Row count should be 35")
+	}
+
+	// each row columns count should be 6
+	for i := range rows {
+		if len(rows[i]) != 7 {
+			t.Fatalf("Row %d column count is %d, should be 7", i, len(rows[i]))
+		}
+	}
+
+	if rows[6][0] != "Gi1/7" {
+		t.Fatalf("Row 7 column 1 should be 'Gi1/7', but got '%s'", rows[6][0])
+	}
+	if rows[27][6] != "Unknown GBIC" {
+		t.Fatalf("Row 27 col 6 should be 'Unknown GBIC', but got '%s'", rows[27][6])
+	}
+	if rows[31][3] != "0" {
+		t.Fatalf("Row 31 (%s) col 3 should be '0', but got '%s'", rows[31][0], rows[31][3])
+	}
+	if rows[32][1] != "" {
+		t.Fatalf("Row 34 col 1 should be '', but got '%s'", rows[32][1])
+	}
+
+	/*for i := range rows {
+		fmt.Printf("'%s|%s|%s|%s|%s|%s|%s'\n", rows[i][0], rows[i][1],rows[i][2],rows[i][3],rows[i][4],rows[i][5],rows[i][6])
+	}*/
+}
+
+func Test_ParseShLldpNeiMES(t *testing.T) {
+	output := `sh lldp neighbors
+
+System capability legend:
+B - Bridge; R - Router; W - Wlan Access Point; T - telephone;
+D - DOCSIS Cable Device; H - Host; r - Repeater;
+TP - Two Ports MAC Relay; S - S-VLAN; C - C-VLAN; O - Other
+
+  Port        Device ID        Port ID       System Name    Capabilities  TTL
+--------- ----------------- ------------- ----------------- ------------ -----
+te1/0/1   ac:f1:df:a4:ae:00 ac:f1:df:a4:a                        O        106
+                            e:1c
+te1/0/4   e0:d9:e3:ba:9e:80    te1/0/3                           O        100
+
+`
+	rows := ParseTable(output, `^-----`, "")
+
+/*	for i := range rows {
+		fmt.Printf("'%s|%s|%s|%s|%s|%s'\n", rows[i][0], rows[i][1],rows[i][2],rows[i][3],rows[i][4],rows[i][5])
+	}*/
+
+	if len(rows) != 2 {
+		t.Fatalf("Expected 2 rows, got %d", len(rows))
+	}
+	for i := range rows {
+		if len(rows[i]) != 6 {
+			t.Fatalf("Row %d: expected 6 cols, got %d", i, len(rows[i]))
+		}
+	}
+	if rows[0][2] != "ac:f1:df:a4:ae:1c" {
+		t.Fatalf("Splitted column is broken ('%s')", rows[0][2])
+	}
+}
