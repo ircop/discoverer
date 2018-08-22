9921d620dca37c2103ce31fc9a2a58681bcb9f4a
ircop <wingman@ip-home.net>
2018-08-22 21:49:48 +0300

    eltex:lldp

---
+++ EltexMES/lldp.go
@@ -0,0 +1,42 @@
+package EltexMES
+
+import (
+	"github.com/ircop/discoverer/base"
+	"github.com/pkg/errors"
+	"github.com/ircop/discoverer/util/text"
+)
+
+// GetLldp for EltexMES profile
+func (p *Profile) GetLldp() ([]discoverer.LldpNeighborship, error) {
+	p.Debug("starting EltexMES.GetLldp()")
+	neighbors := make([]discoverer.LldpNeighborship, 0)
+
+	result, err := p.Cli.Cmd("sh lldp neighbors")
+	if err != nil {
+		return neighbors, errors.Wrap(err, "Cannot get lldp neighborship")
+	}
+	p.Debug(result)
+
+	rows := text.ParseTable(result, "^-----", "")
+	for _, row := range rows {
+		ifname := row[0]
+		cid := row[1]
+		pid := row[2]
+		if ifname == "" || cid == "" || pid == "" {
+			p.Log("LLDP: Warning: empty ifname/cid/pid:(%s/%s/%s)", ifname, cid, pid)
+			continue
+		}
+
+		member := discoverer.LldpNeighbor{
+			PortID:pid,
+			ChassisID:cid,
+		}
+		item := discoverer.LldpNeighborship{
+			PortName:ifname,
+			Members: []discoverer.LldpNeighbor{member},
+		}
+		neighbors = append(neighbors, item)
+	}
+
+	return neighbors, nil
+}
