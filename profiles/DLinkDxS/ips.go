f39effbd7551dcdb3845ae890214eda0427fcf43
ircop <wingman@ip-home.net>
2018-11-06 14:50:05 +0300

    dlink ips

--- profiles/DLinkDxS/ips.go
+++ profiles/DLinkDxS/ips.go
@@ -13,85 +13,73 @@ func (p *Profile) GetIps() ([]*dproto.Ipif, error) {
 	ipifs := make([]*dproto.Ipif, 0)
 	p.Log("Starting DLinkDxS.GetIps()")
 
+	patterns := make(map[string]string)
+	patterns["vlan"] = `(?msi:^VLAN( name)?\s+:(\s+)(?P<vlan>[^\s\n]+))`
+	patterns["ip"] = `(?msi:^ip(v4)? address\s+:\s+(?P<ip>\b(?:\d{1,3}\.){3}\d{1,3}\b)(\s|\n))`
+	patterns["mask"] = `(?msi:^subnet mask\s+:\s+(?P<mask>\b(?:\d{1,3}\.){3}\d{1,3}\b)(\s|\n))`
+	patterns["cidr"] = `(?msi:^ip(v4)? address\s+:\s+(?P<cidr>\b(?:\d{1,3}\.){3}\d{1,3}\b\/\d+)(\s|\n))`
+	regexps, err := p.CompileRegexps(patterns)
+	if err != nil {
+		return ipifs, fmt.Errorf("Cannot compile regexps: %s", err.Error())
+	}
+
 	result, err := p.Cli.Cmd("show ipif")
 	if err != nil {
 		return ipifs, fmt.Errorf("Error during 'show ipif': %s", err.Error())
 	}
 	p.Debug(result)
 
+	result = strings.ToLower(result)
+	parts := strings.Split(result, "ip interface")
 
-	re, err := regexp.Compile(`(?:Interface Name|IP Interface)\s+:\s+(?P<ifname>\S+)\s*\nIP Address\s+:\s+(?P<ip>\S+)\s+\(\S+\)\s*\nSubnet Mask\s+:\s+(?P<mask>\S+)\s*\n(Interface Admin[^\n]+\n)?VLAN Name\s+:\s+(?P<vlan_name>\S+)\s*\n`)
-	if err != nil {
-		return ipifs, fmt.Errorf("Cannot compile ipifs regex")
-	}
-	re2, err := regexp.Compile(`(?:Interface Name|IP Interface)\s+:\s+(?P<ifname>\S+)\s*\nVLAN Name\s+:\s+(?P<vlan_name>\S+)\s*\n([^\n]+\n)?([^\n]+\n)?IP(v4)? Address\s+:\s+(?P<cidr>\S+)\s+\(\S+\)\s*\n`)
-	if err != nil {
-		return ipifs, fmt.Errorf("Cannot compile ipifs regex")
-	}
-
-	out := p.ParseMultiple(re, result)
-	if len(out) > 0 {
-		for _, iface := range out {
-			name := strings.Trim(iface["ifname"], " ")
-			ipstring := strings.Trim(iface["ip"], " ")
-			maskstring := strings.Trim(iface["mask"], " ")
-			vlan := strings.Trim(iface["vlan_name"], " ")
-			if name == "" || ipstring == "" || maskstring == "" || vlan == "" {
-				p.Log("Something wrong: no name/ip/mask/vlan for IPIF (%s/%s/%s/%s)", name, ipstring, maskstring, vlan)
-				continue
-			}
-
-			ip := net.ParseIP(ipstring)
-			if ip == nil {
-				p.Log("Error: wrong ip address '%s'", ipstring)
-				continue
-			}
-			mask := net.ParseIP(maskstring)
-			if mask == nil {
-				p.Log("Error: wrong mask '%s'", maskstring)
-			}
-
-			Interface := dproto.Ipif{
-				Interface: vlan,
-				IP:        ip.String(),
-				Mask:      mask.String(),
-			}
-			ipifs = append(ipifs, &Interface)
+	for i := range parts {
+		part := parts[i]
+		out := p.ParseSingle(regexps["vlan"], part)
+		vlan := strings.Trim(out["vlan"], " ")
+		if vlan == "" {
+			continue
 		}
-	}
 
-	//3200 c1
-	out = p.ParseMultiple(re2, result)
-	if len(out) > 0 {
-		for _, iface := range out {
-			name := strings.Trim(iface["ifname"], " ")
-			cidrString := strings.Trim(iface["cidr"], " ")
-			vlan := strings.Trim(iface["vlan_name"], " ")
-			if name == "" || cidrString == "" || vlan == "" {
-				p.Log("Something wrong: no name/cidr/vlan for IPIF (%s/%s/%s/%s)", name, cidrString, vlan)
-				continue
-			}
+		out = p.ParseSingle(regexps["ip"], part)
+		ipstring := strings.Trim(out["ip"], " ")
+		out = p.ParseSingle(regexps["mask"], part)
+		maskstring := strings.Trim(out["mask"], " ")
+		out = p.ParseSingle(regexps["cidr"], part)
+		cidr := strings.Trim(out["cidr"], " ")
 
-			ip, ipnet, err := net.ParseCIDR(cidrString)
-			if err != nil {
-				p.Log("Cannot parse cidr '%s': %s", cidrString, err.Error())
-				continue
-			}
-
-			maskString := fmt.Sprintf("%d.%d.%d.%d", ipnet.Mask[0], ipnet.Mask[1], ipnet.Mask[2], ipnet.Mask[3])
-			mask := net.ParseIP(maskString)
-			if mask == nil {
-				continue
-			}
-			intf := dproto.Ipif{
-				Interface:vlan,
-				IP:ip.String(),
-				Mask:mask.String(),
-			}
-
-			ipifs = append(ipifs, &intf)
+		if cidr == "" && (maskstring == "" && ipstring == "") {
+			continue
 		}
+
+		iface := dproto.Ipif{
+			Interface:vlan,
+		}
+
+		// find out ip&mask
+		if cidr != "" {
+			ip, net, err := net.ParseCIDR(cidr)
+			if err == nil {
+				iface.IP = ip.String()
+				mask := fmt.Sprintf("%d.%d.%d.%d", net.Mask[0], net.Mask[1], net.Mask[2], net.Mask[3])
+				iface.Mask = mask
+				ipifs = append(ipifs, &iface)
+				continue
+			}
+		}
+
+		ip := net.ParseIP(ipstring)
+		if ip == nil {
+			continue
+		}
+		mask := net.ParseIP(maskstring)
+		if mask == nil {
+			continue
+		}
+		iface.IP = ip.String()
+		iface.Mask = mask.String()
+		ipifs = append(ipifs, &iface)
 	}
 
 	return ipifs, nil
 }
+
